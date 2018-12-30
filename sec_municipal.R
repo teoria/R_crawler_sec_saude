e6672f9dad65a30e9adabd45504eeef547e7ec0f
Rodrigo Carneiro <teoria@gmail.com>
2018-12-30 04:04:04 -0200

    file path

--- sec_municipal.R
+++ sec_municipal.R
@@ -4,21 +4,27 @@ library(rvest)
 saveCOSMS <- function(estado){
         url_cosms <- paste("http://www.conasems.org.br/rede-conasems/rede-cosems-busca/?uf=",estado,sep = "")
         page <- read_html(url_cosms)
+        data <- html_nodes(page ,"body")
         path <- paste("./secretaria_saude_municipal/",estado,"/cosms", sep="")
         if(!dir.exists(path) ){
                 dir.create(path, showWarnings = TRUE, recursive = TRUE, mode = "0777")
         }
-        write(html_estado, paste(path,"/cosms_",estado,".html",sep = "") )
+        pathFile <- paste(path,"/cosms_",estado,".html",sep = "")
+        html_estado <- paste(  paste(data ,collapse = ""), sep = "")
+        write(html_estado, pathFile)
 }
 
-saveCapital<- function(estado){
-        url_capital <- "http://www.conasems.org.br/rede-conasems/capital-busca/?uf=pe"
+saveCapital <- function(estado){
+        url_capital <- paste("http://www.conasems.org.br/rede-conasems/capital-busca/?uf=",estado,sep="")
         page <- read_html(url_capital)
         path <- paste("./secretaria_saude_municipal/",estado,"/capital", sep="")
         if(!dir.exists(path) ){
                 dir.create(path, showWarnings = TRUE, recursive = TRUE, mode = "0777")
         }
-        write(html_estado, paste(path,"/capital_",estado,".html",sep = "") )
+         
+        pathFile <- paste(path,"/capital_",estado,".html",sep = "")
+        html_estado <- paste(  paste(page ,collapse = ""), sep = "")
+        write(html_estado, pathFile)
 }
 
 saveMunicipios<- function(estado){
@@ -38,8 +44,10 @@ saveMunicipios<- function(estado){
                 path <- paste("./secretaria_saude_municipal/",estado,"/municipios", sep="")
                 if(!dir.exists(path) ){
                         dir.create(path, showWarnings = TRUE, recursive = TRUE, mode = "0777")
-                }
-                write(html_estado, paste(path,"/",nomes[i],".html",sep = "") )
+                } 
+                pathFile <- paste(path,"/",nomes[i],".html",sep = "")
+                html_estado <- paste(  paste(page ,collapse = ""), sep = "")
+                write(html_estado, pathFile)
         }
        
 }
@@ -51,10 +59,11 @@ page <- read_html(url_nome_estados)
 lista_estado_html <- html_nodes(page, ".lista_de_estados a")
 lista_estado <-html_attr(lista_estado_html,"data-estado")
 
-for(i in 1:length(lista_estado)){
-        if(!is.na(lista_estado[i])){
+for(k in 1:length(lista_estado)){
+        
+        estado <- lista_estado[k]
+        if(!is.na(estado)){
                 
-                estado <- lista_estado[i]
                 
                 path <- paste("./secretaria_saude_municipal/",estado,sep="")
                 if(!dir.exists(path) ){
