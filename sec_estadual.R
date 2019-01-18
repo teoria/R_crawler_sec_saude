e59659683ad5a5a3f155c6b95f46f8471739766c
Rodrigo <rodrigo.carneiro@ahgora.com.br>
2019-01-18 11:31:01 -0200

    fix

--- sec_estadual.R
+++ sec_estadual.R
@@ -6,13 +6,28 @@ estados_html <- html_nodes(page, ".link-estados")
 estados<-html_attr(estados_html,"href")
 nomes <- html_text(estados_html)
 
-path <- "./secretarias_saude"
-dir.create(path, showWarnings = TRUE, recursive = FALSE, mode = "0777")
-
-for(i in 1:length(estados)){
-        estado2 <- read_html(estados[i])
-        estados_html <- html_nodes(estado2, "main")
-        html_estado <- paste("<div>", paste(html_nodes(estados_html, "p") ,collapse = ""),"</div>", sep = "")
-        write(html_estado, paste("./secretarias_saude/",nomes[i],".html",sep = "") )
+getData_sec_estadual<- function(estados,nomes){
+        for(i in 1:length(estados)){
+                estado2 <- read_html(estados[i])
+                estados_html <- html_nodes(estado2, "main")
+                html_estado <- paste("<div>", paste(html_nodes(estados_html, "p") ,collapse = ""),"</div>", sep = "")
+                write(html_estado, paste("./secretarias_saude/",nomes[i],".html",sep = "") )
+        }    
 }
+
+parseData_sec_estadual <- function(estados,nomes){
+        for(i in 1:length(estados)){
+                path <- paste("./secretarias_saude/",nomes[i],".html",sep = "") 
+                data <- read_html(path)
+                estados_html <- html_nodes(data, "p")
+        }   
+}
+
+
+path <- "./secretarias_saude"
+if( !dir.exists(path)){
+        dir.create(path, showWarnings = TRUE, recursive = FALSE, mode = "0777")
+        getData_sec_estadual(estados,nomes)
+}
+
  
